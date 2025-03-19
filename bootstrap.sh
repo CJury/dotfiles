0f6a3a71d8368eb81f206f4144cc479a7ff67be3
Christopher Jury <christopher.p.j.jury@gmail.com>
2025-03-19 22:29:38 +1100

    Link dotfiles not in config

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,47 +5,64 @@ cd "$(dirname "${BASH_SOURCE}")";
 git config --global url."https://".insteadOf git://
 git submodule update --init --recursive
 
+overwrite_all=false
+overwrite_none=false
+
 function link_config() {
-  local overwrite_all=false
-  local overwrite_none=false
   for path in .config/*; do
     local dst="$HOME/$path"
-    if [ -e "$dst" ]; then
-      if $overwrite_none; then
-        continue
-      fi
-      if ! $overwrite_all; then
-        local overwrite=false
-        echo "$dst already exists, overwrite?"
-        select answer in "Yes" "No" "All" "None"; do
-          case "$answer" in
-            Yes ) overwrite=true ; break ;;
-            No ) overwrite=false; break ;;
-            All )
-              overwrite_all=true 
-              overwrite=true
-              break
-              ;;
-            None )
-              overwrite_none=true
-              overwrite=false
-              break
-              ;;
-          esac
-        done
-      fi
-      if $overwrite; then
-        echo "overwriting $dst"
-        rm -rf "$dst"
-      fi
-    fi
 
-    ln -s "$(pwd)/$path" "$dst"
+    link_file "$(pwd)/$path" "$dst"
   done
+}
+
+function link_file() {
+  local src="$1"
+  local dst="$2"
+
+  if [ -e "$dst" ]; then
+    if $overwrite_none; then
+      return
+    fi
+    if ! $overwrite_all; then
+      local overwrite=false
+      echo "$dst already exists, overwrite?"
+      select answer in "Yes" "No" "All" "None"; do
+        case "$answer" in
+          Yes ) overwrite=true ; break ;;
+          No ) overwrite=false; break ;;
+          All )
+            overwrite_all=true
+            overwrite=true
+            break
+            ;;
+          None )
+            overwrite_none=true
+            overwrite=false
+            break
+            ;;
+        esac
+      done
+    fi
+    if $overwrite; then
+      echo "overwriting $dst"
+      rm -rf "$dst"
+    fi
+  fi
+
+  ln -s "$src" "$dst"
+
+}
+
+function link_dotfiles() {
+  link_file "$(pwd)/.Xresources" "$HOME/.Xresources"
+  link_file "$(pwd)/.tmux.conf" "$HOME/.tmux.conf"
+
 }
 
 function install() {
     link_config
+    link_dotfiles
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
